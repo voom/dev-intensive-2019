e293db84710a132a351faadd80898b34b309dc87
v.potapov <v.potapov@mielooandalexander.com>
2019-06-27 22:30:37 +0200

    task2.2: add message types to model

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -0,0 +1,11 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+class ImageMessage(id: String, from: User?, chat: Chat, isIncoming: Boolean = false, date: Date, var image: String?) :
+    BaseMessage(id, from, chat, isIncoming, date) {
+
+    override fun formatMessage(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
