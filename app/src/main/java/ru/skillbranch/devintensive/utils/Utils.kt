e622493058c51f0a7cfbe4536c21db5a7fd641d3
v.potapov <v.potapov@mielooandalexander.com>
2019-07-02 01:49:59 +0200

    task2.3: extend use case coverage of Utils.parseFullName method

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,7 +2,7 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val split = fullName?.split(' ')
+        val split = fullName?.split(Regex("\\s+"))
         val first = split?.getOrNull(0).takeIf { !it.isNullOrBlank()}
         val last = split?.getOrNull(1).takeIf { !it.isNullOrBlank()}
         return Pair(first, last)
