60a1c1813300ce7ad36a9e3a1214ba9a5b543191
v.potapov <v.potapov@mielooandalexander.com>
2019-07-02 01:42:53 +0200

    task2.7: add Utils.transliteration implementation

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -9,7 +9,81 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return payload
+            .replace(" ", divider)
+            .split("")
+            .map { s -> transliterate(s) }
+            .joinToString("")
+    }
+
+    private fun transliterate(s: String): String {
+        return when (s) {
+            "а" -> "a"
+            "А" -> "A"
+            "б" -> "b"
+            "Б" -> "B"
+            "в" -> "v"
+            "В" -> "V"
+            "г" -> "g"
+            "Г" -> "G"
+            "д" -> "d"
+            "Д" -> "D"
+            "е" -> "e"
+            "Е" -> "E"
+            "ё" -> "e"
+            "Ё" -> "E"
+            "ж" -> "zh"
+            "Ж" -> "Zh"
+            "з" -> "z"
+            "З" -> "Z"
+            "и" -> "i"
+            "И" -> "I"
+            "й" -> "i"
+            "Й" -> "I"
+            "к" -> "k"
+            "К" -> "K"
+            "л" -> "l"
+            "Л" -> "L"
+            "м" -> "m"
+            "М" -> "M"
+            "н" -> "n"
+            "Н" -> "N"
+            "о" -> "o"
+            "О" -> "O"
+            "п" -> "p"
+            "П" -> "P"
+            "р" -> "r"
+            "Р" -> "R"
+            "с" -> "s"
+            "С" -> "S"
+            "т" -> "t"
+            "Т" -> "T"
+            "у" -> "u"
+            "У" -> "U"
+            "ф" -> "f"
+            "Ф" -> "F"
+            "х" -> "h"
+            "Х" -> "H"
+            "ц" -> "c"
+            "Ц" -> "C"
+            "ч" -> "ch"
+            "Ч" -> "Ch"
+            "ш" -> "sh"
+            "Ш" -> "Sh"
+            "щ" -> "sh'"
+            "Щ" -> "Sh'"
+            "ъ" -> ""
+            "ы" -> "i"
+            "Ы" -> "I"
+            "ь" -> ""
+            "э" -> "e"
+            "Э" -> "E"
+            "ю" -> "yu"
+            "Ю" -> "Yu"
+            "я" -> "ya"
+            "Я" -> "Ya"
+            else -> s
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
