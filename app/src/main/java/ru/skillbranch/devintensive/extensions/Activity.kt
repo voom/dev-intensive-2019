45fcf93bef80e0ba18ac25328c468c2d9c728670
v.potapov <v.potapov@mielooandalexander.com>
2019-07-22 00:36:08 +0200

    task3.7: add activity extensions to check whether soft keyboard is visible

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,7 +2,10 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToLong
 
 fun Activity.hideKeyboard() {
     val focus = this.currentFocus
@@ -10,4 +13,17 @@ fun Activity.hideKeyboard() {
         (getSystemService(Context.INPUT_METHOD_SERVICE)
                 as? InputMethodManager)?.hideSoftInputFromWindow(focus.windowToken, 0)
     }
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val rootView = findViewById<View>(android.R.id.content)
+    val visibleBounds = Rect()
+    rootView.getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = rootView.height - visibleBounds.height()
+    val marginOfError = this.convertDpToPixel(50F, this.applicationContext).roundToLong()
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return this.isKeyboardOpen().not()
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Context.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+fun Context.convertDpToPixel(dp: Float, context: Context): Float {
+    return dp * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+}
+
+fun Context.convertPixelsToDp(px: Float, context: Context): Float {
+    return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+}
